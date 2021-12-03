93c5381fbc58c6b7856913514fa7284123fe53e9
DarienPerez <dapec_290@hotmail.com>
2021-12-03 05:17:03 -0600

    Se añaden funciones de validación de campos

--- codigo.js
+++ codigo.js
@@ -28,3 +28,34 @@ $('#formLogin').submit(function(e) {
         });
     }
 });
+
+$('#formSignup').submit(function(e) {
+    e.preventDefault();
+    var usuario = $.trim($("#usuario").val());
+    var password = $.trim($("#password").val());
+
+    if (usuario.length == "" || password == "") {
+        Swal.fire({
+            type: 'warning',
+            title: 'Debe ingresar un usuario y/o password',
+        });
+        return false;
+    } else {
+        $.ajax({
+            url: "../bd/login.php",
+            type: "POST",
+            datatype: "json",
+            data: { usuario: usuario, password: password },
+            success: function(data) {
+                if (data == "null") {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'Usuario y/o contraseña incorrectas',
+                    });
+                } else {
+                    window.location.href = "../dashboard/index.php";
+                }
+            }
+        });
+    }
+});
